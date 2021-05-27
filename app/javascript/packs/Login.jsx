49acdb5da78f4157a10af46fba4e7777e7cd1e8a
anuskapant <anuska.pant@gmail.com>
2021-05-27 16:46:16 +0545

    convert login page in functional component

--- app/javascript/packs/Login.jsx
+++ app/javascript/packs/Login.jsx
@@ -1,44 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 // reactstrap components
 import { 
-  Button, Card, CardHeader, CardBody, CardFooter, CardTitle, FormGroup, Row, Form, Input, InputGroupAddon, InputGroupText, InputGroup, Container, Col,
+  Button, Card, CardHeader, CardBody, CardFooter, CardTitle, FormGroup,
+  Form, Input, InputGroupAddon, InputGroupText, InputGroup, Col,
 } from 'reactstrap';
 import axios from 'axios';
 import NotificationAlert from 'react-notification-alert';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  let notificationAlert;
 
-  componentDidMount() {
+  useEffect(() => {
     document.body.classList.toggle('login-page');
-  }
+    return () => {
+      document.body.classList.toggle('login-page');
+    }
+  }, []);
 
-  componentWillUnmount() {
-    document.body.classList.toggle('login-page');
-  }
-
-  handleChange =(event) => {
-    const { target } = event;
-    const { value } = target;
-    const { name } = target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit =(event) => {
+  const handleSubmit = (event) => {
     const csrf = document.querySelector("meta[name='csrf-token']").getAttribute('content');
     event.preventDefault();
 
-    const { email, password } = this.state;
     axios({
       method: 'post',
       url: '/users/sign_in.json',
@@ -53,14 +39,14 @@ class Login extends React.Component {
       if (response.status === 201) {
         window.location.href = '/';
       } else {
-        this.notify('bc');
+        notify('bc');
       }
     }).catch(() => {
-      this.notify('bc');
+      notify('bc');
     });
   }
 
-  notify = (place) => {
+  const notify = (place) => {
     const type = 'danger';
     let options = {};
     options = {
@@ -68,64 +54,63 @@ class Login extends React.Component {
       message: (
         <div>
           <div>
+            <i className='tim-icons icon-bell-55 pr-2' />
             Login Unsuccessful!
           </div>
         </div>
       ),
       type,
-      icon: 'tim-icons icon-bell-55',
+      icon: '',
       autoDismiss: 7,
     };
-    this.notificationAlert.notificationAlert(options);
+    notificationAlert.notificationAlert(options);
   }
 
-  render() {
-    return (
-      <>
-        <div className="rna-container">
-          <NotificationAlert ref={(n) => { this.notificationAlert = n; }} />
-        </div>
-        <div className="bg-gradient-info">
-          <div class="container d-flex flex-column min-vh-100 justify-content-center align-items-center">
-            <Col lg="4" md="6" className="mx-auto my-auto">
-              <Card className="bg-white">
-                <CardHeader className="bg-transparent">
-                  <CardTitle tag="h1">Log in</CardTitle>
-                </CardHeader>
-                <CardBody>
-                  <Form role="form">
-                    <FormGroup>
+  return (
+    <>
+      <div className="rna-container">
+        <NotificationAlert ref={(n) => { notificationAlert = n; }} />
+      </div>
+      <div className="bg-gradient-info">
+        <div className="container d-flex flex-column min-vh-100 justify-content-center align-items-center">
+          <Col lg="4" md="6" className="mx-auto my-auto">
+            <Card className="bg-white">
+              <CardHeader className="bg-transparent">
+                <CardTitle tag="h1">Log in</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Form role="form">
+                  <FormGroup>
+                  <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="tim-icons icon-email-85" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Email" type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+                  </InputGroup>
                     <InputGroup>
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="tim-icons icon-email-85" />
+                          <i className="tim-icons icon-lock-circle" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input placeholder="Email" type="text" name="email" onChange={this.handleChange} />
+                      <Input placeholder="Password" type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
                     </InputGroup>
-                      <InputGroup>
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="tim-icons icon-lock-circle" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input placeholder="Password" type="password" name="password" onChange={this.handleChange} />
-                      </InputGroup>
-                    </FormGroup>
-                  </Form>
-                </CardBody>
-                <CardFooter>
-                <Button block className="mb-3" color="primary" onClick={this.handleSubmit} size="lg">
-                  Get Started
-                </Button>
-                </CardFooter>
-              </Card>
-            </Col>
-          </div>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <CardFooter>
+              <Button block className="mb-3" color="primary" onClick={handleSubmit} size="lg">
+                Get Started
+              </Button>
+              </CardFooter>
+            </Card>
+          </Col>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 document.addEventListener('DOMContentLoaded', () => {
