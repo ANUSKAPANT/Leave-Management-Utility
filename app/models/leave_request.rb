1b35f6c330ee11952e82f11be8bc8dad9f324f03
sagar535 <070bct535@ioe.edu.np>
2021-12-20 13:19:41 +0545

    adds new column leave_type to LeaveRequest

--- app/models/leave_request.rb
+++ app/models/leave_request.rb
@@ -1,14 +1,17 @@
 class LeaveRequest < ApplicationRecord
   belongs_to :user
   enum status: { pending: 0, approved: 1, rejected: 2 }
+  enum leave_type: { sick_leave: 0, personal: 1, others: 2 }
 
+  validates_presence_of :title
+  validates_presence_of :leave_type, message: "Leave Type can't be blank"
   # validate same user can't create leave for same day with multiple reasons
   validate :prevent_multiple_leave_on_same_day
 
   private
 
   def prevent_multiple_leave_on_same_day
-    user.upcoming_leaves.each do |leave|
+    (user.upcoming_leaves - [self]).each do |leave|
       if overlaps?(leave)
          return self.errors.add :message, "Leave already exists for given day/s"
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_091116) do
+ActiveRecord::Schema.define(version: 2021_12_20_034901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_12_17_091116) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "leave_type"
   end
 
   create_table "replies", force: :cascade do |t|
